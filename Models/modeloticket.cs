e693c913c76b792fc1f9be7768950e2d2903bae1
CesarValenzuela157 <cesar.play30@gmail.com>
2025-04-29 23:14:53 -0400

    cambios ticket
    Cambios ticket en los controladores y modelos

--- Models/modeloticket.cs
+++ Models/modeloticket.cs
@@ -10,9 +10,13 @@ public class Ticket
     public int IdTicket { get; set; }
     public string Titulo { get; set; }
     public string Descripcion { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public int IdUsuario { get; set; }
     public int IdEstado { get; set; }
+    
+    public int IdCategoria { get; set; }
     public int IdPrioridad { get; set; }
     public int IdDepartamento { get; set; }
-    public DateTime FechaCreacion { get; set; }
+    
 }
 

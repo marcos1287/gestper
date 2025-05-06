0ad931e5c80d1449267dfdbcf6ab5f1688fd14ee
CesarValenzuela157 <cesar.play30@gmail.com>
2025-05-05 22:39:26 -0400

    Mejoras de Admin

--- Models/modeloarchivoadjunto.cs
+++ Models/modeloarchivoadjunto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gestper.Models;
 
 public class modeloarchivoadjunto
@@ -7,6 +9,7 @@ public class modeloarchivoadjunto
 
 public class Archivoadjunto
 {
+    [Key]
     public int Idarchivo { get; set; }
     public int IDticket { get; set; }
     public string Idusuario { get; set; }
--- Models/modeloasignacion.cs
+++ Models/modeloasignacion.cs
@@ -1,13 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gestper.Models;
 
-public class modeloasignacion
-{
-    
-}
 
 public class Asignacion
 {
-    public int IdAsignacion { get; set; }
+    [Key] public int IdAsignacion { get; set; }
+    
     public string Idticket { get; set; }
     public string Idusuario { get; set; }
     public DateTime Feshaasignacion { get; set; }

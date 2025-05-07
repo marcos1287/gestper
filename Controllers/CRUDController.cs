c128ef9eb35a5e2d90a165bd27e6340dce220a7c
Nacho14072002 <ignaciojaramillo20002@gmail.com>
2025-05-06 20:03:24 -0400

    coso

--- Controllers/CRUDController.cs
+++ Controllers/CRUDController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Gestper.Data;
+using Gestper.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Gestper.Controllers
 {
@@ -14,6 +18,31 @@ namespace Gestper.Controllers
             }
 
             return View("Views/CRUD/crud.index.cshtml");
+        }
+        
+        private readonly ApplicationDbContext _context;
+
+        public CRUDController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Perfil()
+        {
+            var usuarioCorreo = HttpContext.Session.GetString("UsuarioCorreo");
+            if (string.IsNullOrEmpty(usuarioCorreo))
+            {
+                return RedirectToAction("Login", "Usuario");
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(u => u.Correo == usuarioCorreo);
+
+            if (usuario == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View("crud.perfil", usuario);
         }
     }
 }

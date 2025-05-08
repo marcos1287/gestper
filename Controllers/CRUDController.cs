d4a3d67111b92292be4484be0b37178bbcb0e59a
CesarValenzuela157 <cesar.play30@gmail.com>
2025-05-08 08:11:37 -0400

    Mejoras de ticket en controlador y visualizacion correspondiente

--- Controllers/CRUDController.cs
+++ Controllers/CRUDController.cs
@@ -45,5 +45,31 @@ namespace Gestper.Controllers
             
             return View("crud.perfil", usuario);
         }
+        public async Task<IActionResult> TicketsCreados()
+        {
+            var correo = HttpContext.Session.GetString("UsuarioCorreo");
+            if (string.IsNullOrEmpty(correo))
+            {
+                return RedirectToAction("Login", "Usuario");
+            }
+
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Correo == correo);
+
+            if (usuario == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var tickets = await _context.Tickets
+                .Include(t => t.Estado)
+                .Include(t => t.Categoria)
+                .Include(t => t.Prioridad)
+                .Include(t => t.Departamento)
+                .Where(t => t.IdUsuario == usuario.IdUsuario)
+                .ToListAsync();
+
+            return View("Views/CRUD/crud.ticket.cshtml", tickets); 
+        }
+
     }
 }
